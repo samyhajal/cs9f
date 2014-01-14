26f593b148720391aeec5fd2e9dcbc4521aa35a2
samyhajal <hajalsamy@gmail.com>
2014-01-14 10:15:28 -0800

    Create prog1.cpp

---
+++ prog1.cpp
@@ -0,0 +1,24 @@
+// custom countdown using while
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int curr, prev, total, subtotal;
+    subtotal = 0;
+    total = 0;
+    while (true) {
+        cin >> curr;
+        subtotal += curr;
+    	total += curr;
+    	if (curr == 0 and prev != 0) {
+            cout << "Subtotal " << subtotal <<endl;
+    	    subtotal=0;}
+    	if (curr == 0 and prev == 0) {
+            cout << "Total " << total <<endl;
+    		break;
+        }
+    	prev = curr;
+    }
+	return 0;
+}
